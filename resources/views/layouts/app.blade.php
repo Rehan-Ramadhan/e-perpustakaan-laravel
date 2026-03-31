acdccb8d9c422ea60ecc83f1dc5ea5d994d7be9b
Rehan Ramadhan <rehanramadhan06092008@gmail.com>
2026-03-31 09:37:07 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title') | {{ config('app.name', 'e-perpus') }}</title>
@@ -20,39 +19,41 @@
     <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
 
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+
     <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
     <script src="{{ asset('assets/js/config.js') }}"></script>
-
 </head>
 
-<body>
-    <body class="bg-light">
-        <div class="layout-wrapper layout-content-navbar layout-without-menu">
-            <div class="layout-container">
+<body class="bg-light">
+    <div class="layout-wrapper layout-content-navbar layout-without-menu">
+        <div class="layout-container">
 
-                <div class="layout-page" style="padding-left: 0 !important;">
+            <div class="layout-page" style="padding-left: 0 !important;">
 
-                    @include('bagian.navbar')
+                @include('bagian.navbar')
 
-                    <div class="content-wrapper">
-                        <div class="container-xxl flex-grow-1 container-p-y">
-                            @yield('content')
-                        </div>
-
-                        @include('bagian.footer')
+                <div class="content-wrapper">
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        @yield('content')
                     </div>
+                    @include('bagian.footer')
+
+                    <div class="content-backdrop fade"></div>
                 </div>
             </div>
         </div>
-    </body>
-    </div>
 
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
     <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
     <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+
     <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    @stack('scripts')
 </body>
 
 </html>
