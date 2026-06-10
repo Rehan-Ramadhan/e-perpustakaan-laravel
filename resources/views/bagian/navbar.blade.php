1ba1409bd5520c6f735c61d851a8b1ce3cabb09f
Rehan Ramadhan <rehanramadhan06092008@gmail.com>
2026-06-10 22:49:01 +0700

    fix: navbar

--- resources/views/bagian/navbar.blade.php
+++ resources/views/bagian/navbar.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container-fluid">
-        <div class="row py-3 border-bottom">
+        <div class="row py-3 border-bottom align-items-center">
             <div class="col-sm-4 col-lg-3 text-center text-sm-start">
                 <div class="main-logo">
                     <a href="{{ route('home') }}"
@@ -15,27 +15,15 @@
                 </div>
             </div>
 
-            <div class="col-sm-6 offset-sm-2 offset-md-0 col-lg-5 d-none d-lg-block">
+            <div class="col-sm-12 col-lg-5 d-none d-lg-block">
                 <div class="search-bar row bg-light p-2 my-2 rounded-4">
-                    <div class="col-md-3 d-none d-md-block">
-                        <div class="bg-white rounded-pill px-2 py-1 shadow-sm border">
-                            <select class="form-select border-0 bg-transparent fw-bold text-muted pe-0"
-                                style="box-shadow: none; cursor: pointer; font-size: 0.85rem; min-width: 100%;">
-                                <option selected disabled>Kategori</option>
-                                @foreach($kategori as $k)
-                                    <option value="{{ $k->id }}">{{ $k->nama }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="col-11 col-md-9">
+                    <div class="col-12">
                         <form id="search-form"
-                            class="d-flex align-items-center bg-white rounded-pill px-3 py-1 shadow-sm"
+                            class="d-flex align-items-center bg-white rounded-pill px-3 py-1 shadow-sm w-100"
                             action="{{ route('katalog.index') }}" method="get">
-                            <input type="text" name="q" class="form-control border-0 bg-transparent me-2"
+                            <input type="text" name="q" class="form-control border-0 bg-transparent me-2 flex-grow-1"
                                 placeholder="Cari buku..." value="{{ request('q') }}" style="box-shadow: none;">
-                            <button type="submit" class="btn border-0 p-0 text-muted ms-1">
+                            <button type="submit" class="btn border-0 p-0 text-muted ms-1 flex-shrink-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
                                     <path fill="currentColor"
                                         d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z" />
